facdf4f4d6f9df176a2cbc66f8a02943f4de1d49
Hao Guo <jennnnyguo@gmail.com>
2023-07-04 19:05:29 +1000

    finish Palindrome Lab Solution 1

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+function reverseString(word) {
+  return word.split("").reverse().join("")
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverseString(word)
+  return word === reversedWord
 }
 
 /* 
-  Add your pseudocode here
+ 反转输入的string
+
+ 如果反转后的string和反转前的字符串相同
+ 返回true
+ 不然
+ 返回false
 */
 
 /*
@@ -20,6 +30,21 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("jenny"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("leon"));
 }
 
 module.exports = isPalindrome;
